ee2346b91b5ce8efa08a024e3ad3605f5e605298
akira-omran <kirabot77@gamil.com>
2025-08-26 14:32:25 +0300

    change ANON_KEY

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://tdsaxesjwrjtcxznbnda.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkc2F4ZXNqd3JqdGN4em5ibmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNDg5ODksImV4cCI6MjA3MTYyNDk4OX0'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkc2F4ZXNqd3JqdGN4em5ibmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNDg5ODksImV4cCI6MjA3MTYyNDk4OX0.S9e30ScivJa4M56vyvIKWzdyq87K6tnEABb2yKZsCrE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
